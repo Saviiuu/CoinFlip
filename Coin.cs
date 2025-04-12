5e65f5ed37cd6e5803827d61eec40513353ed0ec
Sávio <saviovinysb@gmail.com>
2025-04-11 22:36:47 -0300

    COINFLIP ATUALIZAÇÃO

--- Coin.cs
+++ Coin.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CoinFlip
 {
-    internal class Coin
+    public class Coin
     {
+        private string ladoSorteado;
+
+        public string LadoSorteado { get => ladoSorteado; set => ladoSorteado = value; }
+
+        public Coin()
+        {
+        
+        
+        }
+
+        public string Flip()
+        {
+            Random random = new Random();
+           
+            LadoSorteado = random.Next(2) == 0 ? "Cara" : "Coroa";
+
+            return (LadoSorteado);
+        }
+
     }
 }
